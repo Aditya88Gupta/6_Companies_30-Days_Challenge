0012a7d0d6789e15025e80b2564409fbe14c2b7b
Aditya88Gupta <prince88aditya@gmail.com>
2022-01-08 20:13:47 +0530

    Day 3

---
+++ Amazon/First non-repeating character in a stream.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    // Code here
+		    queue<int> q;
+		    vector<int> Count(26,0);
+		    for(int i=0;i<A.size();i++){
+		        Count[A[i]-'a']++;
+		        if(Count[A[i]-'a']==1)
+		            q.push(A[i]);
+		        while(!q.empty() && Count[q.front()-'a']!=1)
+		            q.pop();
+		        if(q.empty())
+		            A[i]='#';
+		        else
+		            A[i]=q.front();
+		    }
+		    return A;
+		}
+
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string A;
+		cin >> A;
+		Solution obj;
+		string ans = obj.FirstNonRepeating(A);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends

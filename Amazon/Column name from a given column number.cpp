19c5a0600a1a1b5fc4819a1ce268bd32d4ca7270
Aditya88Gupta <prince88aditya@gmail.com>
2022-01-13 00:34:28 +0530

    Update Column name from a given column number.cpp

--- Amazon/Column name from a given column number.cpp
+++ Amazon/Column name from a given column number.cpp
@@ -1,14 +1,37 @@
-class Solution {
-  public:
-    string colName (long long int n) {
-        string s = "";
-        while(n != 0) {
-            // To make the number 0 indexed
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        // Extract alphabets recursively, much like we extract digits
+        string res="";
+        while(n!=0){
+            // zero based idx for when n%26==0
             n--;
-            int r = n % 26;
-            s = string(1, r + 'A') + s;
-            n = n / 26;
+            int tmp = n%26;
+            res=char(tmp+65)+res;
+            n=n/26;    
         }
-        return s;
+        return res;
+        
     }
 };
+
+// { Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+  // } Driver Code Ends

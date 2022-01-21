e3f414d0a2bce7811ed33674b81bf8bf0ffe2d6b
Aditya88Gupta <prince88aditya@gmail.com>
2022-01-21 23:13:06 +0530

    Update Implement Atoi.cpp

--- Adobe/Implement Atoi.cpp
+++ Adobe/Implement Atoi.cpp
@@ -1,28 +1,45 @@
-int atoi(string str) {
-      int ans=0;
-      int n=str.size();
-      bool check=0;
+// { Driver Code Starts
+//Initial template for C++
 
+#include <bits/stdc++.h>
+using namespace std;
 
-      if(str[0]=='-')
-        check=1;
-      else{
-          ans=ans*10 + (str[0]-'0');
-      }     
+ // } Driver Code Ends
+//User function template for C++
 
+class Solution{
+  public:
+    /*You are required to complete this method */
+    int atoi(string str) {
+        //Your code here
+        bool negv=false;
+        int num=0;
+        int tmp=1;
+        for(int i=str.length()-1;i>=0;i--){
+            if(isdigit(str[i])){
+                num = num + (str[i]-'0')*tmp;
+                tmp=tmp*10;
+            }else if(i==0 && str[i]=='-')
+                negv=true;
+            else
+                return -1;
+        }
+        if(negv)
+            return -num;
+        return num;
+    }
+};
 
-      if(n==1 && check==0)
-           return ans;
-      else if(n==1 && check==1)
-           return -1;
-           
-      for(int i=1;i<n;i++){
-          if(str[i]>='0' && str[i]<='9')
-                ans=ans*10 + (str[i]-'0');
-          else
-               return -1;
-      }
-      if(check==0)
-           return ans;
-      return (0-ans);
-}
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>s;
+		Solution ob;
+		cout<<ob.atoi(s)<<endl;
+	}
+}  // } Driver Code Ends

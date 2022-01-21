7be921d31035cc333274a2ae2e73c2673d1fb659
Aditya88Gupta <prince88aditya@gmail.com>
2022-01-21 23:35:50 +0530

    Update Pots of Gold Game.cpp

--- Adobe/Pots of Gold Game.cpp
+++ Adobe/Pots of Gold Game.cpp
@@ -1,23 +1,60 @@
-int helper(vector<int>&v,int start,int end,vector<vector<int>>&dp)
-    {
-        if(start>end)
-        {
-            return dp[start][end]=0;
-        }
-        if(start==end)
-        {
-            return dp[start][end]=v[start];
-        }
-        if(dp[start][end]!=-1)
-        {
-            return dp[start][end];
-        }
-        int first=v[start]+min(helper(v,start+1,end-1,dp),helper(v,start+2,end,dp));
-        int second=v[end]+min(helper(v,start+1,end-1,dp),helper(v,start,end-2,dp));
-        return dp[start][end]=max(first,second);
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+    
+    vector<vector<int>> dpTable;
+public:
+
+    int optimalCoins(int i, int j, vector<int>&A){
+        if(i>j)
+            return 0;
+        if(i==j)
+            return A[i];
+        if(i+1==j)
+            return max(A[i],A[j]);
+        if(dpTable[i][j]==0){
+            // cuz opponent will also choose optimally, we have to 
+            // choose the optimal path in which the opponent will score minimum
+            int tmp1= A[i]+min(optimalCoins(i+2,j,A),optimalCoins(i+1,j-1,A));
+            int tmp2= A[j]+min(optimalCoins(i+1,j-1,A),optimalCoins(i,j-2,A));
+            dpTable[i][j]=max(tmp1,tmp2);
+        }   
+        return dpTable[i][j];
     }
+    
     int maxCoins(vector<int>&A,int n)
     {
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-    return helper(A,0,n-1,dp);
+	    //Write your code here
+	    for(int i=0;i<n;i++){
+	        dpTable.push_back(vector<int>());
+	        for(int j=0;j<n;j++){
+	            dpTable[i].push_back(0);
+	        }
+	    }
+	    
+	    return optimalCoins(0,n-1,A);
     }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int>A(N);
+        for (int i = 0; i < N; i++) {
+            cin >> A[i];
+        }
+        Solution ob;
+        cout << ob.maxCoins(A, N) << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends

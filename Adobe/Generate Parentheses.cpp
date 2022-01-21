5b36c71b626942aede4c7a164acb3a24460443dc
Aditya88Gupta <prince88aditya@gmail.com>
2022-01-21 23:13:12 +0530

    Update Generate Parentheses.cpp

--- Adobe/Generate Parentheses.cpp
+++ Adobe/Generate Parentheses.cpp
@@ -1,19 +1,75 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<string> AllParenthesis(int n) ;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+// N is the number of pairs of parentheses
+// Return list of all combinations of balanced parantheses
 class Solution
 {
+    vector<string> res;
     public:
-    vector<string> v;
-    void rec(string s,int n,int opened)
-    {
-        if(n!=0)
-            rec(s+"(",n-1,opened+1);
-        if(opened!=0)
-            rec(s+")",n,opened-1);
-        if(n==0 && opened==0)
-            v.push_back(s);
+    
+    // bool checkBalance(string str){
+    //     stack<char> st;
+    //     for(int i=0;i<str.length();i++){
+    //         if(str[i]==')'){
+    //             if(st.empty())
+    //                 return false;
+    //             st.pop();
+    //         }else
+    //             st.push(str[i]);
+    //     }
+    //     if(st.empty())
+    //         return true;
+    //     return false;    
+    // }
+    
+    void possibleComb(string str, int openCount, int closeCount){
+        if(openCount==0 && closeCount==0){
+            //cout<<str<<endl;
+            res.push_back(str);
+        }else{
+            if(openCount!=0){
+                possibleComb(str+'(',openCount-1,closeCount);
+            }
+            if(closeCount>openCount){
+                possibleComb(str+')',openCount,closeCount-1);
+            }
+        }
     }
+    
     vector<string> AllParenthesis(int n) 
     {
-        rec("",n,0);
-        return v;
+        // Your code goes here 
+        possibleComb("",n,n);
+        return res;
     }
 };
+
+// { Driver Code Starts.
+
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		Solution ob;
+		vector<string> result = ob.AllParenthesis(n); 
+		sort(result.begin(),result.end());
+		for (int i = 0; i < result.size(); ++i)
+			cout<<result[i]<<"\n";
+	}
+	return 0; 
+} 
+  // } Driver Code Ends

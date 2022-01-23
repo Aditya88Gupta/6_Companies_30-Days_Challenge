d7b06591281955f77d398d7244dc18e838715ea8
Aditya88Gupta <prince88aditya@gmail.com>
2022-01-23 18:17:48 +0530

    Update Amend The Sentence.cpp

--- Adobe/Amend The Sentence.cpp
+++ Adobe/Amend The Sentence.cpp
@@ -1,35 +1,42 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
 class Solution{
     public:
     string amendSentence (string s)
     {
         // your code here
-        if(s.size() == 0) return s;
-        vector<string> v;
-        string temp = "";
-        for(int i=0; i<s.size(); i++){
-            char c = s[i];
-            if(c >= 'A' and c <= 'Z'){
-                v.push_back(temp);
-                temp = "";
-                temp += c;
-            } else {
-                temp += c;
+        string res="";
+        int i=0;
+        s[0]=toupper(s[0]);
+        while(i<s.length()){
+            if(isupper(s[i])){
+                res+=tolower(s[i]);
+                i++;
+                while(i<s.length() && !isupper(s[i])){
+                    res+=s[i];
+                    i++;
+                }
+                if(i!=s.length())
+                    res+=" ";    
             }
         }
-        v.push_back(temp);
-        
-        string res = "";
-        
-        for(auto str:v){
-            string t = "";
-            for(int i=0; i<str.size(); i++){
-                t += tolower(str[i]);
-            }
-            res += t + " ";
-        }
-        
-        if(res[0] == ' ') res.erase(0, 1);
-        if(res[res.size()-1] == ' ') res.erase(res.size()-1, res.size());
         return res;
     }
 };
+
+// { Driver Code Starts.
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		string s; cin >> s;
+		Solution ob;
+		cout << ob.amendSentence (s) << endl;
+	}
+}  // } Driver Code Ends

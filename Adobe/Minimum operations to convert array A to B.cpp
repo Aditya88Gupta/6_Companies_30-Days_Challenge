7fb2edce3b58f432c7b3ad056b3df09f16bd19b6
Aditya88Gupta <prince88aditya@gmail.com>
2022-01-23 22:07:37 +0530

    Update Minimum operations to convert array A to B.cpp

--- Adobe/Minimum operations to convert array A to B.cpp
+++ Adobe/Minimum operations to convert array A to B.cpp
@@ -8,35 +8,76 @@ using namespace std;
 
 class Solution {
   public:
+  
+    // void printMat(vector<vector<int>> dpTable){
+    //     for(int i=0;i<dpTable.size();i++){
+    //         for(int j=0;j<dpTable[i].size();j++)
+    //             cout<<dpTable[i][j]<<" ";
+    //         cout<<endl;    
+    //     }
+    // }
+    
+    // bool search(int ele, int B[], int M){
+    //     int low=0;
+    //     int high=M-1;
+    //     while(low<=high){
+    //         int mid = low + (high-low);
+    //         if(B[mid]==ele)
+    //             return true;
+    //         if(B[mid]>ele)
+    //             high=mid-1;
+    //         else
+    //             low=mid+1;
+    //     }
+    //     return false;
+    // }
+    
+    int longestSubsequence(int n, vector<int> a)
+    {
+        if(n==0)
+            return 0;
+       // last element of active subsquences
+       vector<int> tailEle(n,0);
+       // next empty slot
+       int length=1;
+       tailEle[0]=a[0];
+       for(int i=1;i<n;i++){
+           if(a[i]>tailEle[length-1])
+               // Extend the longest active subsquence
+               tailEle[length++]=a[i];
+           else if(a[i]<tailEle[0])
+               // new smallest element can kick off a subsquence
+               tailEle[0]=a[i];
+           else{
+               // iter will point towards the subsquence which can be extended using a[i]
+               auto iter = lower_bound(tailEle.begin(),tailEle.begin()+length,a[i]);
+               *iter = a[i];
+           }   
+       }
+       return length;
+    }
+    
     int minInsAndDel(int A[], int B[], int N, int M) {
         // code here
-        vector<int> tmp;
-        sort(A,A+N);
-        tmp.push_back(A[0]);
-        int i=1;
-        while(i<N){
-            if(A[i-1]!=A[i])
-                tmp.push_back(A[i]);
-            i+=1;    
+        vector<int> A_updated;
+        unordered_set<int> B_set;
+        int minOp(0);
+        for(int i = 0; i < M; i++) 
+            B_set.insert(B[i]);
+            
+        for(int i=0;i<N;i++){
+            // O(1) search
+            if(B_set.find(A[i])!=B_set.end())
+                A_updated.push_back(A[i]);
+            else
+                minOp+=1;
         }
-        int opCount = N - tmp.size();
-        i = 0;
-        int j = 0;
-        while(i<M && j<tmp.size()){
-            if(tmp[j]==B[i]){
-                j++;
-                i++;
-            }else if(tmp[j]<B[i]){
-                opCount+=1;
-                j++;
-            }else{
-                opCount+=1;
-                i++;
-            }
-        }
-        opCount+=(M-i);
-        opCount+=tmp.size()-j;
-        return opCount;
+        
+        int length = longestSubsequence(A_updated.size(),A_updated);
+        //cout<<tmp.size()<<endl;
+        
+        minOp+= (M-length) + (A_updated.size()-length);
+        return minOp;
     }
 };
 

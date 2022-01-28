422fb25c7fcf8a8f378e2558bb0e38139622ac55
Aditya88Gupta <prince88aditya@gmail.com>
2022-01-28 23:44:52 +0530

    Update Number of Boomerangs.cpp

--- Intuit/Number of Boomerangs.cpp
+++ Intuit/Number of Boomerangs.cpp
@@ -1,18 +1,32 @@
 class Solution {
-public:
-  int numberOfBoomerangs(vector<vector<int>>& P) {
-    vector<unordered_map<int,int>>table(P.size());
+  public:
     
-    int ans = 0;
-    for(int i = 0; i != P.size(); i++){
-      for(int j = i+1; j != P.size(); j++){
-        int dx = P[i][0] - P[j][0], dy = P[i][1] - P[j][1];
-        int dis = dx*dx + dy*dy;
-        table[i][dis]++, table[j][dis]++; 
-      }
-      for(auto [d, n]: table[i]) ans += n*(n-1);
+    double calDist(int x1, int y1, int x2, int y2){
+        int xDiff = abs(x1-x2);
+        int yDiff = abs(y1-y2);
+        return sqrt(xDiff*xDiff+yDiff*yDiff);
     }
-              
-    return ans;  
-  }
+    
+    int numberOfBoomerangs(vector<vector<int>>& points) {
+        if(points.size()<3)
+            return 0;
+        int count=0;
+        unordered_map<int,unordered_map<double,int>> Map;
+        for(int i=0;i<points.size();i++){
+            for(int j=i+1;j<points.size();j++){
+                double dist = calDist(points[i][0],points[i][1],points[j][0],points[j][1]);
+                Map[i][dist]++;
+                Map[j][dist]++;
+            }
+        }
+        
+        for(int i=0;i<points.size();i++){
+            for(auto iter=Map[i].begin();iter!=Map[i].end();iter++){
+                //cout<<iter->first<<" "<<iter->second<<endl;
+                if(iter->second>=2)
+                    count+= (iter->second)*(iter->second-1);
+            }
+        }
+        return count;
+    }
 };

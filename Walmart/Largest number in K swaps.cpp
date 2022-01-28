c1e660a4d2fd3ac91bf11b46f5b58a1716aa688e
Aditya88Gupta <prince88aditya@gmail.com>
2022-01-28 23:03:01 +0530

    Day 3

---
+++ Walmart/Largest number in K swaps.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    string Max;
+    public:
+    
+    void swap(string &str, int i, int j){
+        char ch = str[i];
+        str[i] = str[j];
+        str[j] = ch;
+    }
+    
+    void findMax(string str,int k,int pos){
+        
+        if(str>Max)
+            Max=str;
+        
+        if(k==0)
+            return;
+            
+        char curMax = str[pos];    
+        for(int i=pos+1;i<str.length();i++){
+            if(curMax<str[i])
+                curMax=str[i];
+        }
+        
+        if(curMax!=str[pos])
+            k--;
+        
+        for(int j=str.length()-1;j>=pos;j--){
+            if(str[j]==curMax){ 
+                swap(str,pos,j);
+                findMax(str,k,pos+1);
+                swap(str,pos,j);
+            }
+        }
+    }
+    
+    
+    string findMaximumNum(string str, int k)
+    {
+       // code here.
+       findMax(str,k,0);
+       return Max;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t, k;
+    string str;
+
+    cin >> t;
+    while (t--)
+    {
+        cin >> k >> str;
+        Solution ob;
+        cout<< ob.findMaximumNum(str, k) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends

05cf8df6249de351823088e11bce03ef919daa2d
Aditya88Gupta <prince88aditya@gmail.com>
2022-01-28 22:32:17 +0530

    Update Largest number in K swaps.cpp

--- Intuit/Largest number in K swaps.cpp
+++ Intuit/Largest number in K swaps.cpp
@@ -1,55 +1,74 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
 class Solution
 {
+    string Max;
     public:
-    void match(string& str, string& res)
-    {
-        for(int i=0; i<str.size(); i++)
-        {
-            if( res[i] > str[i] )
-                return;
-            if( res[i] < str[i] )
-            {
-                res = str;
-                return;
+    
+    void swap(string &str, int i, int j){
+        char ch = str[i];
+        str[i] = str[j];
+        str[j] = ch;
+    }
+    
+    void findMax(string str,int k,int pos){
+        
+        if(str>Max)
+            Max=str;
+        
+        if(k==0)
+            return;
+            
+        // find maximum number > pos    
+        char curMax = str[pos];    
+        for(int i=pos+1;i<str.length();i++){
+            if(curMax<str[i])
+                curMax=str[i];
+        }
+        
+        if(curMax!=str[pos])
+            k--;
+        
+        // which idx swap would result in max
+        for(int j=str.length()-1;j>=pos;j--){
+            if(str[j]==curMax){ 
+                swap(str,pos,j);
+                findMax(str,k,pos+1);
+                // back track
+                swap(str,pos,j);
             }
         }
     }
-
-    public:
-    void setDigit(string& str, int index, string& res, int k)
-    {
-        // base case
-        if(k==0 || index==str.size()-1)
-        {
-            match(str,res);
-            return;
-        }
-        
-        int maxDigit = 0;
-        for(int i=index; i<str.size(); i++)
-            maxDigit = max( maxDigit, str[i]-'0' );
-        if( str[index]-'0' == maxDigit )
-        {
-            setDigit(str, index+1, res, k);
-            return;
-        }
-        
-        for(int i=index+1; i<str.size(); i++)
-        {
-            if( str[i]-'0' == maxDigit ){
-                swap( str[index] , str[i] );
-                setDigit(str, index+1, res, k-1);
-                swap( str[index] , str[i] );
-            }
-        }
-    }
-
-    public:
+    
+    
     string findMaximumNum(string str, int k)
     {
-        string res = str;
-        setDigit(str, 0, res, k);
-        return res;
+       // code here.
+       findMax(str,k,0);
+       return Max;
     }
-
 };
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t, k;
+    string str;
+
+    cin >> t;
+    while (t--)
+    {
+        cin >> k >> str;
+        Solution ob;
+        cout<< ob.findMaximumNum(str, k) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends

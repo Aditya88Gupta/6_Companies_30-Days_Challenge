bc4ca9e417646bf4d377d0e7fc8f2d2c89eaa613
Aditya88Gupta <prince88aditya@gmail.com>
2022-01-31 00:01:56 +0530

    Update Divide Two Integers.cpp

--- Walmart/Divide Two Integers.cpp
+++ Walmart/Divide Two Integers.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        int divide(int dividend, int divisor) {
-        int q=0;
-        int Sum = 0;
-        if(divisor==1)
-            return dividend;
-        if(divisor==-1){
-            if(dividend==INT_MIN)
-                return INT_MAX;
-            return -dividend;
+        if(!divisor) return INT_MAX;
+        if(divisor == 1) return dividend;
+        if(divisor == -1){
+            if(dividend == INT_MIN) {return INT_MAX;}
+            else {return -dividend;}
         }
-        while(Sum<=abs(dividend)-abs(divisor)){
-            Sum+=abs(divisor);
+        int q=0;
+        int num = abs(dividend);
+        int div = abs(divisor);
+        while(num-div>=0){
+            num-=div;
             q++;    
         }
         if(dividend<0 && divisor<0 || dividend>0 && divisor>0)
             return q;
         else
             return -q;
-    }
     }
 };

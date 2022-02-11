945d4d6c038cd35eebcde841aa4977db138861e7
Aditya88Gupta <prince88aditya@gmail.com>
2022-02-11 19:10:28 +0530

    Update Number of distict Words with k maximum contiguous vowels.cpp

--- Adobe/Number of distict Words with k maximum contiguous vowels.cpp
+++ Adobe/Number of distict Words with k maximum contiguous vowels.cpp
@@ -1,82 +1,71 @@
-#include <bits/stdc++.h>
-using namespace std;
+// { Driver Code Starts
+//Initial Template for C++
 
-long long int power(long long int x,
-                    long long int y,
-                    long long int p)
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+#define mod 1000000007;
+
+class Solution
 {
-    long long int res = 1ll;
-
-    x = x % p;
-
-    if (x == 0)
-        return 0;
-
-    while (y > 0) {
-
-        if (y & 1)
-            res = (res * x) % p;
-        y = y >> 1;
-        x = (x * x) % p;
-    }
-    return res;
-}
-
-// Function for finding number of ways to
-// create string with length N and atmost
-// K contiguous vowels
-int kvowelwords(int N, int K)
-{
-
-    long long int i, j;
-    long long int MOD = 1000000007;
-
-    // Array dp to store number of ways
-    long long int dp[N + 1][K + 1] = { 0 };
-
-    long long int sum = 1;
-    for (i = 1; i <= N; i++) {
-
-        // dp[i][0] = (dp[i-1][0]+dp[i-1][1]..dp[i-1][k])*21
-        dp[i][0] = sum * 21;
-        dp[i][0] %= MOD;
-
-        // Now setting sum to be dp[i][0]
-        sum = dp[i][0];
-
-        for (j = 1; j <= K; j++) {
-            // If j>i, no ways are possible to create
-            // a string with length i and vowel j
-            if (j > i)
-                dp[i][j] = 0;
-            else if (j == i) {
-                // If j = i all the character should
-                // be vowel
-                dp[i][j] = power(5ll, i, MOD);
-            }
-            else {
-                // dp[i][j] relation with dp[i-1][j-1]
-                dp[i][j] = dp[i - 1][j - 1] * 5;
-            }
-
-            dp[i][j] %= MOD;
-
-            // Adding dp[i][j] in the sum
-            sum += dp[i][j];
-            sum %= MOD;
+  public:
+    
+    void printMat(vector<vector<long long>> dpTable){
+        for(int i=0;i<dpTable.size();i++){
+            for(int j=0;j<dpTable[i].size();j++)
+                cout<<dpTable[i][j]<<" ";
+            cout<<endl;    
         }
     }
+    
+    int kvowelwords(int N, int K) {
+        // total vowels in a word can be > K
+        vector<vector<long long>> dpTable(N+1,vector<long long>(K+1,0));
+        for(int i=0;i<=N;i++){
+            for(int j=0;j<=K;j++){
+                // no places available
+                if(i==0){
+                    dpTable[i][j]=1;
+                }
+                else{
+                    // Add a consonant at the new place
+                    dpTable[i][j] = (dpTable[i-1][K]*21)%mod;
+                    // dpTable[i-1][K] = distinct words of len i-1 with K max contiguous vowels
+                    if(j>0){
+                        // Add a vowel at the new place
+                        // j-1 cuz if new vowel is added to a section of contiguous vowels
+                        // total wil still be <= j
+                        long long tmp = (dpTable[i-1][j-1]*5)%mod;
+                        dpTable[i][j] = (dpTable[i][j]+tmp)%mod;
+                    }
+                }
+            }
+        }
+        printMat(dpTable);
+        return dpTable[N][K];
+    }
+};
 
-    return sum;
-}
-// Driver Program
+
+// { Driver Code Starts.
+
+
 int main()
 {
-    // Input
-    int N = 3;
-    int K = 3;
-
-    // Function Call
-    cout << kvowelwords(N, K) << endl;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;int K;
+        cin >>N>>K;
+        
+        Solution ob;
+        int ans = ob.kvowelwords(N,K);
+        cout << ans << endl;
+    }
     return 0;
-}
+}  // } Driver Code Ends

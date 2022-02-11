b900a895b86cda7bd42247b92b8c5d771ee9387e
Aditya88Gupta <prince88aditya@gmail.com>
2022-02-11 22:42:45 +0530

    Update Bridge edge in a graph.cpp

--- Microsoft/Bridge edge in a graph.cpp
+++ Microsoft/Bridge edge in a graph.cpp
@@ -1,33 +1,58 @@
-void DFS(vector<int> adj[], int v, bool visited[])
-	{
-        visited[v] = true;
-        vector<int>::iterator i;
-        for (i = adj[v].begin(); i != adj[v].end(); ++i)
-            if (!visited[*i]) 
-                DFS(adj, *i, visited);
-    }
-    bool isConnected(vector<int> adj[], int V, int one, int two) 
-    {
-        bool visited[V] = {0};
-        memset(visited, false, sizeof(visited));
-        DFS(adj, one, visited);
-        if (visited[two] == false) 
-            return false;
-        return true;
-    }
-    
-    int isBridge(int V, vector<int> adj[], int c, int d)
-    {
-        if (!isConnected(adj, V, c, d))
-            return 0;
-        else
-        {
-            adj[c].erase(remove(adj[c].begin(), adj[c].end(), d), adj[c].end());
-            adj[d].erase(remove(adj[d].begin(), adj[d].end(), c), adj[d].end());
-            if (isConnected(adj, V, c, d))
-                return 0;
-            else
-                return 1;
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+	public:
+    void dfs(int vertex, vector<int> adj[], int c, int d, vector<bool>& Visited){
+        Visited[vertex]=true;
+        for(int i=0;i<adj[vertex].size();i++){
+            if(Visited[adj[vertex][i]]==false){
+                if((vertex!=c || adj[vertex][i]!=d))
+                    dfs(adj[vertex][i],adj,c,d,Visited);    
+            }
         }
     }
+    
+    
+    int isBridge(int V, vector<int> adj[], int c, int d) 
+    {
+        // Code here
+        vector<bool> Visited(V,false);
+        dfs(c,adj,c,d,Visited);
+        return !Visited[d];        
+    }
 };
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back (v);
+            adj[v].push_back (u);
+        }
+        
+        int c,d;
+        cin>>c>>d;
+        
+        Solution obj;
+    	cout << obj.isBridge(V, adj, c, d) << "\n";
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
